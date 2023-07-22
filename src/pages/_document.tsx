da914de6c0802d6d60579ef5de754c0753da3c90
BU-RI <proaca779@gmail.com>
2023-07-22 20:26:43 +0900

    ぷｓｈ

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,8 +2,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" className='haikei'>
       <Head />
+      <title>あめりかんどっぐ</title>
       <body>
         <Main />
         <NextScript />
---
+++ src/pages/secondtest.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import {Layout} from "../components/Layout"
+
+export default function About() {
+    return (
+      <Layout>
+    <h1 className="example">本番ページ</h1>
+      </Layout> 
+    )
+  }
