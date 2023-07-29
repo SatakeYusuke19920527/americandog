d83ba5b3759dffdf64252456c35ad3648996e6f1
BU-RI <proaca779@gmail.com>
2023-07-29 20:59:16 +0900

    本番に絵を張りました

--- src/pages/secondtest.tsx
+++ src/pages/secondtest.tsx
@@ -4,7 +4,12 @@ import {Layout} from "../components/Layout"
 export default function About() {
     return (
       <Layout>
+        <div>
     <h1 className="example">本番ページ</h1>
+    <div className='honban'>
+    <img className='honbanimg' src="/front.png"/>
+    </div>
+    </div>
       </Layout> 
     )
   }
