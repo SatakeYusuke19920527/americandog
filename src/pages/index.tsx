a761221091779367dc64757ad54188b2fca2051d
BU-RI <proaca779@gmail.com>
2023-08-05 20:07:07 +0900

    もういちどpushします

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,15 @@
+import { useState } from "react";
 import { Main } from "next/document";
 import {Layout} from "../components/Layout"
 import Link from 'next/link';
 
 export default function Home() {
+  const [isChecked, setIsChecked] = useState(false)
+
+  const toggleCheckbox = () => {
+    setIsChecked(!isChecked)
+  }
+
   return (
     <Layout>
     <div>
@@ -12,7 +19,9 @@ export default function Home() {
       あめりかんどっぐ
     </div>
     {/* 利用規約に同意しないと入れないようにする */}
-    <Link href="/secondtest"><button>ゲームページに飛ぶ</button></Link>
+    <Link href="/secondtest"><button type="submit" disabled={!isChecked}>ゲームページへ飛ぶ</button></Link>
+    <br/><p　className="example">※チェックすると利用規約に同意したことになります</p><input type="checkbox" onChange={toggleCheckbox}></input>
+    <br/><Link href=""　className="example">利用規約ページへ飛ぶ</Link>
     </main>
     </div>
     </Layout>
