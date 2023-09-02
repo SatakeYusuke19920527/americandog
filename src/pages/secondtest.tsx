9c61d7740220d340fbbded9006bf9356236b2b93
SatakeYusuke19920527 <satkyusk@icloud.com>
2023-09-02 20:56:08 +0900

    oo

--- src/pages/secondtest.tsx
+++ src/pages/secondtest.tsx
@@ -1,3 +1,4 @@
+import { imageConfigDefault } from 'next/dist/shared/lib/image-config'
 import Image from 'next/image'
 import {Layout} from "../components/Layout"
 
@@ -7,7 +8,8 @@ export default function About() {
         <div>
     <h1 className="example">本番ページ</h1>
     <div className='honban'>
-    <img className='honbanimg' src="/front.png"/>
+    {/* <img className='honbanimg' src="/front.png"/> */}
+    <img src="/"/>
     </div>
     </div>
       </Layout> 
