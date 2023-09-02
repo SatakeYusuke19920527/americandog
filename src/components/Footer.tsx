644bf814e2c59e91e831c75b1c2317622a5a8c52
SatakeYusuke19920527 <satkyusk@icloud.com>
2023-09-02 04:36:10 -0700

    tail

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,22 +1,16 @@
-
-
-  const Footer = () => {
+import Link from "next/link"
+export default function Footer() {
     return (
-      <footer>
-        <div className="footer"> 
-        
-        <ul className="footer-menu">
-  
-          </ul>
-          
-         <a className="official"></a>
-          <div className="copyright">
-  <p className="text-white">©︎2023american dog  </p>
-          </div>
-          </div>
-      </footer>
+      <>
+      <div className="text-right">
+      <Link href="/  "　className="text-white text-right">アメリカンドッグ</Link>
+      <Link href="/termsOfUse  "　className="text-white flex-row-reverse">利用規約</Link>
+      </div>
+      <p className="text-white"> ©️americandog</p>
+      </>
     )
-  }
+}
   
-  export default Footer
+  
+  
   
