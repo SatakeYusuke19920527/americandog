3548f0e827f1b815534ffb7f0cb8c4f2db055e7f
SatakeYusuke19920527 <satkyusk@icloud.com>
2023-09-16 20:49:56 +0900

    20230916sawa

--- src/components/Hand.tsx
+++ src/components/Hand.tsx
@@ -4,7 +4,8 @@ export default function Hand() {
   return (
       <>
       <div className="hand">
-      <h1 className="handle">手持ち</h1>
+        {/* <h1 className="handle">手持ち</h1> */}
+        <img className="bed" src="/bed.png"></img>
       </div>
       </>
     )
--- src/pages/secondtest.tsx
+++ src/pages/secondtest.tsx
@@ -2,13 +2,16 @@ import { Layout } from "../components/Layout"
 import { Handle } from "../components/Hand"
 
 export default function About() {
-
+  const buttonAlert = () => {
+    alert('ボタンがクリックされました。');
+  }
     return (
       <Layout>
         <div>
           <h1 className="example">本番ページ</h1>
           <div className='honban'>
             <div className="bg-img">
+              <img className="bed" onClick={buttonAlert} src="/bed.png"></img>
               <Handle>
                 {/* aaa */}
               </Handle>
