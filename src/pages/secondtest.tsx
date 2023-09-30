f146842e177f158bff55449534df6b376f18c3b2
SatakeYusuke19920527 <satkyusk@icloud.com>
2023-09-30 20:58:42 +0900

    0930sawa

--- src/pages/secondtest.tsx
+++ src/pages/secondtest.tsx
@@ -1,21 +1,34 @@
 import { Layout } from "../components/Layout"
 import { Handle } from "../components/Hand"
+import {
+  FacebookShareButton,
+  HatenaShareButton,
+  LineShareButton,
+  TwitterShareButton,
+  FacebookIcon,
+  HatenaIcon,
+  LineIcon,
+  TwitterIcon,
+} from "react-share";
 
 export default function About() {
-  const buttonAlert = () => {
-    alert('ボタンがクリックされました。');
-  }
+    const URL = 'https://localhost:3000/';
+    const QUOTE = 'みんなに共有してね　#ホラー';
+
     return (
       <Layout>
         <div>
           <h1 className="example">本番ページ</h1>
           <div className='honban'>
-            <div className="bg-img">
-              <img className="bed" onClick={buttonAlert} src="/bed.png"></img>
-              <Handle>
-                {/* aaa */}
-              </Handle>
-              </div>
+      <FacebookShareButton url={URL} quote={QUOTE}>
+      <FacebookIcon size={60} round />
+      </FacebookShareButton>
+      <TwitterShareButton url={URL} title={QUOTE}>
+      <TwitterIcon size={60} round />
+      </TwitterShareButton>
+      <LineShareButton url={URL} title={QUOTE} >
+      <LineIcon size={60} round />
+      </LineShareButton>
           </div>
         </div>
       </Layout> 
