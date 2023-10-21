01c10990f3ef9f9f289283588f9641407f80bcc1
SatakeYusuke19920527 <satkyusk@icloud.com>
2023-10-21 04:33:51 -0700

    完了

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,13 @@
 import Link from "next/link"
 export default function Footer() {
     return (
-      <>
+      <footer className="">
       <div className="text-right align-text-bottom">
+      <Link href="/  "　className="text-white w-full">american</Link>
       <Link href="/termsOfUse  "　className="text-white w-full">利用規約</Link>
       </div>
       <p className="text-white text-center"> ©️americandog</p>
-      </>
+      </footer>
     )
 }
   
