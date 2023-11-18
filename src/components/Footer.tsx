e41d6dcdd61180e077cd69eb372645f88dd131fd
SatakeYusuke19920527 <satkyusk@icloud.com>
2023-11-18 20:48:50 +0900

    aaaa

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,11 +2,12 @@ import Link from "next/link"
 export default function Footer() {
     return (
       <footer className="absolute bottom-100% ">
-        <div className=" text-center">
-        <Link href="/  "　className="text-white text-left ">american</Link>
-        <Link href="/termsOfUse  "　className="text-white ">利用規約</Link>
-        </div>
-        <p className="text-white text-center"> ©️americandog</p>
+      <div className=" text-center">
+          <Link href="/" className="text-white text-left ">ホーム画面</Link>
+          <Link href="/secondtest"　className="text-white ">シェア画面</Link>
+          <Link href="/termsOfUse"　className="text-white ">利用規約</Link>
+      </div>
+      <p className="text-white text-center"> ©️americandog</p>
       </footer>
     )
 }
